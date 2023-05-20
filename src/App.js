26c597d040977572e62abc0ba95eef632fdf5f46
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2023-05-21 00:52:58 +1200

    added p tag to the top of the page and padding for some reason needs to be anything more than zero for the top. Added name to the center, background is a nice black

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  document.title = 'Jake G Portfolio'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div class="container">
+        <p>Welcome</p>
+
+        <div className="App">
+          <header className="App-header">
+            <h3>Jake Gardner</h3>
+          </header>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
