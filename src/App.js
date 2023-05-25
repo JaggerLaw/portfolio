8c8ab9361502d99860aebaa5c2e26081de277edc
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2023-05-25 22:56:59 +1200

    added list for nav bar will make it on one line later

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ function App() {
     <>
       <div class="container">
         <p>Welcome</p>
+        <nav class="main-navbar">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
 
         <div className="App">
           <header className="App-header">
