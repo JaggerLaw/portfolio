832fa7220f0ac4ff70a45ed05c62ebc71fca0ca0
JaggerLaw <53713119+JaggerLaw@users.noreply.github.com>
2023-05-26 23:19:14 +1200

    removed embedded repository as this would not show any of the files inside

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  document.title = 'Jake G Portfolio'
+
+  return (
+    <>
+      <div class="container">
+        <p>Welcome</p>
+        <nav class="main-navbar">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+
+        <div className="App">
+          <header className="App-header">
+            <h3>Jake Gardner</h3>
+          </header>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
