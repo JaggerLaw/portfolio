956ed62c78630f32c15a8a6bc1aafd4842147a5e
jake <mamagotbig@gmail.com>
2023-06-01 00:36:33 +1200

    made an about section with a tid bit of me as a developer,also included some css to center this, and lastly added links that go nowhere for the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,35 @@ function App() {
   return (
     <>
       <div class="container">
-        <nav>
-          <ul class="main-navbar">
-            <li>Home</li>
-            <li>About</li>
-            <li>Projects</li>
-            <li>Contact</li>
-          </ul>
-        </nav>
+  <nav>
+    <ul class="main-navbar">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Projects</a>
+      </li>
+      <li>
+        <a href="#">Contacts</a>
+      </li>
+    </ul>
+  </nav>
 
-        <div className="App">
-          <header className="App-header">
-            <h3>Jake Gardner</h3>
-          </header>
-        </div>
-      </div>
+  <div class="App">
+    <header class="App-header">
+      <h3>Jake Gardner</h3>
+    </header>
+  </div>
+
+  <section>
+    <h3>About</h3>
+    <p>I am Jake, a software developer passionate about the development lifecycle. I have proper work experience with worksafe.</p>
+  </section>
+</div>
+
     </>
   )
 }
