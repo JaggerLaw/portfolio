8831137262ce72e31397fb088b1fc9e8aeca57ba
jake <mamagotbig@gmail.com>
2023-06-04 03:00:49 +1200

    took away outline for navbar, added picture of myself to background using css, made it so navbar is overlayed ontop of img, made small semi transparent box around about

--- client/src/App.js
+++ client/src/App.js
@@ -5,38 +5,31 @@ function App() {
 
   return (
     <>
-      <div class="container">
-  <nav>
-    <ul class="main-navbar">
-      <li>
-        <a href="#">Home</a>
-      </li>
-      <li>
-        <a href="#">About</a>
-      </li>
-      <li>
-        <a href="#">Projects</a>
-      </li>
-      <li>
-        <a href="#">Contacts</a>
-      </li>
-    </ul>
-  </nav>
-
-  <div class="App">
-    <header class="App-header">
-      <h3>Jake Gardner</h3>
-    </header>
-  </div>
-
-  <section>
-    <h3>About</h3>
-    <p>I am Jake, a software developer passionate about the development lifecycle. I have proper work experience with worksafe.</p>
-  </section>
-</div>
-
+      <div className="container">
+        <nav>
+          <ul className="main-navbar">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contacts</a>
+            </li>
+          </ul>
+        </nav>
+        <h3>Jake Gardner</h3>
+        <section>
+          <h3>About</h3>
+          <p>I am Jake, a software developer passionate about the development lifecycle. I have proper work experience with worksafe.</p>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
