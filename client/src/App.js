5a2797625647b7dfe68279801da28f04db289c7c
jake <mamagotbig@gmail.com>
2023-06-06 14:56:49 +1200

    made the background image into an actual image tag since i dont need it to be behind everything now, made it circular and have it in the middle of the top part just under the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import logo from './img/gira.jpg';
+import project2 from './img/projectR.jpg';
+import project3 from './img/logo192.png';
+import me from './img/jake-grad_cropped.jpg';
 
 function App() {
   document.title = 'Jake G Portfolio';
@@ -23,6 +26,7 @@ function App() {
             </li>
           </ul>
         </nav>
+        <img className="main-pic" src={me}/>
         <h3>Jake Gardner</h3>
         <section>
           <h3>About</h3>
@@ -35,7 +39,22 @@ function App() {
             <p>Description of Project 1</p>
             <a href="#">Learn More</a>
           </div>
+          <div className="card">
+            <img src={project2}/>
+            <h3>Project 2</h3>
+            <p>Desription of project 2</p>
+            <a href="#">Learn more</a>
+          </div>
+          <div className="card">
+            <img src={project3}/>
+            <h3>Project 3</h3>
+            <p>Desription of project 3</p>
+            <a href="#">Learn more</a>
+          </div>
         </div>
+        <section>
+          contact information
+        </section>
       </div>
     </>
   );
